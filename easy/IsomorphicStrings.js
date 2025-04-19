d3479c5cbefa717d4c75a5255e6b35b4c63f349b
Clint Jones <clinton.alan.jones@gmail.com>
2025-04-19 14:17:28 -0700

    Initial commit

---
+++ easy/IsomorphicStrings.js
@@ -0,0 +1,52 @@
+/**
+ * 205. Isomorphic Strings
+ * 
+ * Given two strings s and t, determine if they are isomorphic.
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ * All occurrences of a character must be replaced with another character while preserving
+ * the order of characters. No two characters may map to the same character, but a character may map to itself.
+ *
+ * @param {string} s first string
+ * @param {string} t second string
+ * @return {boolean} true if the strings are isomorphic, false otherwise
+ */
+var isIsomorphic = function (s, t) {
+  // isomorphic strings must be the same length
+  if (s.length != t.length) {
+      return false;
+  }
+
+  // map strings to chars
+  let sToCharT = {};
+  // reverse map to catch duplicate values
+  let tToCharS = {};
+
+  // iterate through the length of the strings (arbitrarily choosing s)
+  for (let i = 0; i < s.length; i++) {
+      let charS = s[i];
+      let charT = t[i];
+
+      // Check whether s[i] has already been mapped in the dictionary
+      if (sToCharT[charS]) {
+          // The letter is alrady mapped to a different value
+          // we have a mismatch and therefore a non-unique mapping
+          if (sToCharT[charS] !== charT) {
+              return false;
+          }
+      } else {
+          // go ahead and map it
+          sToCharT[charS] = charT;
+      }
+
+      // Now the reverse map
+      if (tToCharS[charT]) {
+          if (tToCharS[charT] !== charS) {
+              return false;
+          }
+      } else {
+          tToCharS[charT] = charS;
+      }
+  }
+
+  return true;
+};
---
+++ easy/RemoveElement.js
@@ -0,0 +1,25 @@
+/**
+ * 27. Remove Element
+ * 
+ * Remove all instances of a given value from an array.
+ * Updating the values in place.
+ * Preserve the order of remaining elements.
+ *
+ * @param {number[]} nums the array of numbers
+ * @param {number} val the number to remove from the array
+ * @return {number} the new length of the array
+ */
+var removeElement = function (nums, val) {
+  let k = 0; // the number of elements that do not match the input value
+  let i = 0; // The index where the unmatched values are set
+
+    for (let x = 0; x < nums.length; x++) {
+      // If the current element is not a match
+      if (val !== nums[x]) {
+          nums[i++] = nums[x]; // add it back to the new index location
+          k++; // update the count of the unmatched elements
+      }
+  }
+
+  return k; // Return the new length of the array with all val items removed
+};
