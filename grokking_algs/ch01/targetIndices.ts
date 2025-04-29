53f4ce4e6c75fc5c3d56a2181a72372b7cb01d08
Clint Jones <clinton.alan.jones@gmail.com>
2025-04-28 21:16:07 -0700

    Solution for returning the matching value's target indices

--- grokking_algs/ch01/targetIndices.ts
+++ grokking_algs/ch01/targetIndices.ts
@@ -11,28 +11,63 @@
  * list. The returned list must be sorted in increasing order.
  *
  * NOTES:
- * Efficiency is O(n log n) for the sort
+ * Efficiency is O(n log n) for the sort. The binary search O(log n) is more
+ * efficient than a linear search O(n) but it won't be noticeable on small sets.
+ * Even in larger sets it wouldn't gain very much because the search efficiency
+ * is eclipsed by the required sort operation O(n log n)
  *
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 function targetIndices(nums: number[], target: number): number[] {
-  let arrTarget: number[] = [];
+   let arrTarget: number[] = [];
 
-  // non-decreasing order (non-lexical)
-  nums.sort( (a, b) => a - b );
+   // Sort in non-decreasing order
+   nums.sort((a, b) => a - b);
 
-  // loop through the sorted list
-  for (let num = 0; num < nums.length; num++) {
-      // find the array values that match the target number
-      if (target === nums[num]) {
-          // add the index to the return array
-          arrTarget.push(num)
+   let low = 0;
+   let high = nums.length - 1;
+
+   // Start the binary search
+   while (low <= high) {
+      let mid = low + Math.floor((high - low) / 2);
+      let current = nums[mid];
+
+      /* If the target number is found, verify if it's the first
+         occurrence (smallest index) */
+      if (target === current) {
+         /* Confirm that this is the first occurrence by checking the
+            previous element */
+         if ( (mid === 0) || (nums[mid - 1] !== target) ) {
+            // add the matching element's index to the return array
+            arrTarget.push(mid);
+
+            // Now we want to find the remaining matches to the right
+            let tempIndex = mid + 1;   // check the next
+            while ( (tempIndex < nums.length) && (nums[tempIndex] === target) ) {
+               arrTarget.push(tempIndex);
+               tempIndex++;
+            }
+
+            // Contains indexes of all matches
+            return arrTarget;
+         } else {
+            // Not the first occurrence, move left to keep looking
+            high = mid - 1;
+         }
+      } else if (target < current) {
+         // Eliminate the right half
+         high = mid - 1;
+      } else {
+         // Eliminate the left half
+         low = mid + 1;
       }
-  }
+   }
 
-  return arrTarget;
-};
+   // No matching values were found if it reaches here.
+   return arrTarget;
+}
+
 
 export default targetIndices;
