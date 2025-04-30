7771db1b3d9873f71368079b7f4b9622a400eafd
Clint Jones <clinton.alan.jones@gmail.com>
2025-04-29 17:10:11 -0700

    Make a sorted array of squares more efficient using 2 pointers instead of an additional sort

--- grokking_algs/ch01/sortedSquares.ts
+++ grokking_algs/ch01/sortedSquares.ts
@@ -5,18 +5,39 @@
  * Given an integer array nums sorted in non-decreasing order, return an
  * array of the squares of each number sorted in non-decreasing order.
  *
- * NOTE:
+ * NOTE: Working from the ends of the input array, right (largest) to left (smallest)
+ * to account for negative numbers (-n) that may be larger than squared positives.
+ * Fill in the result array from right to left
+ *
+ * Efficiency: Using two index pointers (high and low) is O(n),
+ * avoiding the additional cost of sorting O(n log n).
  *
  * @param {number[]} nums - number array sorted in non-decreasing order
  * @return {number[]} - array of squares for each number sorted in
  *    non-decreasing order
  */
 function sortedSquares(nums: number[]): number[] {
-  for (let num = 0; num < nums.length; num++) {
-    nums[num] **= 2;
+  let left = 0;
+  let right = nums.length - 1;
+  let result = new Array(nums.length);
+
+  let position = nums.length - 1;  // Start at the last index
+
+  // Working from the ends of the input array, right (largest) to left (smallest)
+  // Filling in the result array from right to left
+  while (left <= right) {
+    // Account for -n**
+    if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+      result[position] = nums[left] ** 2;
+      left++;  // Move left pointer forward one position
+    } else {
+      result[position] = nums[right] ** 2;
+      right--;  // Move right pointer back one position
+    }
+    position--;  // Fill the next position in the result array
   }
 
-  return nums.sort((a, b) => a - b);
-};
+  return result;
+}
 
 export default sortedSquares;
