74223a4c5803f912f4211d7b1c11bc3528307cff
Clint Jones <clinton.alan.jones@gmail.com>
2025-05-16 14:43:22 -0700

    Prep for LC-350

--- grokking_algs/ch02/intersect.ts
+++ grokking_algs/ch02/intersect.ts
@@ -2,16 +2,19 @@
  * 350. Intersection of Two Arrays II
  *
  * https://leetcode.com/problems/intersection-of-two-arrays-ii/
- * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+ * Given two integer arrays nums1 and nums2, return an array of their intersection.
+ * Each element in the result must appear as many times as it shows in both arrays
+ * and you may return the result in any order.
  *
  * Follow up:
  * - What if the given array is already sorted? How would you optimize your algorithm?
  * - What if nums1's size is small compared to nums2's size? Which algorithm is better?
- * - What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+ * - What if elements of nums2 are stored on disk, and the memory is limited such
+ *   that you cannot load all elements into the memory at once?
  *
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
+ * @param {number[]} nums1 - integer arrays
+ * @param {number[]} nums2 - integer arrays
+ * @return {number[]} - an array of the intersection of nums1 and nums2
  */
 function intersect(nums1: number[], nums2: number[]): number[] {}
 
